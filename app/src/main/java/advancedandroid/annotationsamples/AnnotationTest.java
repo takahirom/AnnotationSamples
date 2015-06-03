d18fa31e644cd5e98f0fa6e6107e2e7e76b80bc8
takahirom <takam.dev@gmail.com>
2015-06-03 23:49:02 +0900

    add background annotation test

---
+++ app/src/main/java/advancedandroid/annotationsamples/AnnotationTest.java
@@ -0,0 +1,16 @@
+package advancedandroid.annotationsamples;
+
+import android.support.annotation.WorkerThread;
+
+/**
+ * Created by takam on 2015/06/03.
+ */
+public class AnnotationTest {
+    public void callBackgroundMethod() {
+        backgroundMethod();
+    }
+    @WorkerThread
+    private void backgroundMethod() {
+        System.out.println("test");
+    }
+}
--- app/src/main/java/advancedandroid/annotationsamples/MainActivity.java
+++ app/src/main/java/advancedandroid/annotationsamples/MainActivity.java
@@ -2,8 +2,7 @@ package advancedandroid.annotationsamples;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.annotation.WorkerThread;
 
 public class MainActivity extends Activity {
 
@@ -11,27 +10,12 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        backgroundMethod();
+        new AnnotationTest().callBackgroundMethod();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    @WorkerThread
+    private void backgroundMethod() {
+        System.out.println("test");
     }
 }
