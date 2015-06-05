b0a67ae5a5f2380c6447c6b12ffe28dc1f47cd52
takahirom <takam.dev@gmail.com>
2015-06-06 00:32:54 +0900

    add multiple annotation

--- app/src/main/java/advancedandroid/annotationsamples/ConstraintsAnnotationSampleActivity.java
+++ app/src/main/java/advancedandroid/annotationsamples/ConstraintsAnnotationSampleActivity.java
@@ -5,20 +5,29 @@ import android.os.Bundle;
 import android.support.annotation.Size;
 import android.util.Log;
 
-public class SizeSampleActivity extends Activity {
+public class ConstraintsAnnotationSampleActivity extends Activity {
     public static final String TAG = "test12345678901234567890";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        logging("test12345678901234567890", "test");
+        logging("test12345678901234567890", "test"); // error
         final String test = "test12345678901234567890";
-        logging(test, "test");
-        logging(TAG, "test");
+        logging(test, "test"); // in 1.3 EAP.0 no error
+        logging(TAG, "test"); // in 1.3 EAP.0 no error
+
+        foo("fo"); // error
+        foo("foo"); // no error
+        foo("fooo"); // error
+        foo("fooooo"); // no error
     }
 
     private void logging(@Size(max=23) String tag, String message) {
         Log.d(tag,message);
+    }
+
+    private void foo(@Size(multiple=3) String size) {
+        System.out.println(size);
     }
 }
